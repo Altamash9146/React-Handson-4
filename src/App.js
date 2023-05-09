5b74ba0682d7ee749125782f772d995163d1232d
sayyedaltamash853@gmail.com <sayyedaltamash853@gmail.com>
2023-05-09 17:15:55 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter,Routes, Route } from "react-router-dom";
+import { ContactUs } from './Components/ContactUs';
+import { Home } from './Components/Home';
+import { Students } from './Components/Students';
+import { Error } from "./Components/Error";
+import { AddStudentData } from './Components/AddStudentData';
+import { UpdateStudentData } from './Components/UpdateStudentData';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+        <Route path='/AddStudentData' element={<AddStudentData/>}/>
+        <Route path='/UpdateStudentData' element={<UpdateStudentData/>}/>
+        <Route path='/' element ={<Home/>}/>
+        <Route path='/Students' element ={<Students/>}/>
+        <Route path='/ContactUS' element ={<ContactUs/>}/>
+        <Route path='/*' element={<Error/>}/> 
+    </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Nav } from './Nav';
+
+export function Home() {
+  return (
+    <>
+      <Nav/>
+    <div className='Home-Heading'>Home Page</div>
+    </>
+  )
+}
+
